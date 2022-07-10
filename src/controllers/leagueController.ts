53580ba62de1e77bdb935ca9f38da3523132268d
cedafiso0403 <cfigueroasocar00@mylangara.ca>
2022-07-10 16:15:07 -0700

    Get all match history

--- src/controllers/leagueController.ts
+++ src/controllers/leagueController.ts
@@ -1,4 +1,4 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import axios from "axios";
 
 const SEASON_12_BEGINS_TIMESTAMP: number = 1641297600;
@@ -36,37 +36,39 @@ type DataToReturn = {
 }
 // Dev KEY
 
-// Get Summoner ID
-const getEncryptID = async (sumName: string, region: string): Promise<any> => {
-  async function sumInfo () {
-      return await axios.get(`https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${sumName}`, {
-        headers: {
-          "X-Riot-Token": process.env.riotAPIKey
-        },
-        params:{
-        }
-  })}
+// Get Summoner info
+const getSummonerInfo = async (sumName: string, region: string): Promise<any> => {
+  async function sumInfo() {
+    return await axios.get(`https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${sumName}`, {
+      headers: {
+        "X-Riot-Token": process.env.riotAPIKey
+      },
+      params: {
+      }
+    })
+  }
   return ((await sumInfo()).data)
 };
 
 const getRankedStatsID = async (sumID: string, region: string): Promise<any> => {
-  async function sumInfo () {
-      return await axios.get(`https://${region}.api.riotgames.com/lol/league/v4/entries/by-summoner/${sumID}`, {
-        headers: {
-          "X-Riot-Token": process.env.riotAPIKey
-        },
-        params:{
-        }
-  })}
+  async function sumInfo() {
+    return await axios.get(`https://${region}.api.riotgames.com/lol/league/v4/entries/by-summoner/${sumID}`, {
+      headers: {
+        "X-Riot-Token": process.env.riotAPIKey
+      },
+      params: {
+      }
+    })
+  }
   return ((await sumInfo()).data)
 };
 
-const findQueueData = ( queuesData: object[], queueRequested: string): object=>{
+const findQueueData = (queuesData: object[], queueRequested: string): object => {
   const data: object = {};
-  const dataFound = queuesData.find((queue: QueueObject)=>{
-    if(queue.queueType === "RANKED_SOLO_5x5" && queueRequested === "Solo"){
+  const dataFound = queuesData.find((queue: QueueObject) => {
+    if (queue.queueType === "RANKED_SOLO_5x5" && queueRequested === "Solo") {
       return queue;
-    }else if(queue.queueType === "RANKED_FLEX_SR" && queueRequested === "Flex"){
+    } else if (queue.queueType === "RANKED_FLEX_SR" && queueRequested === "Flex") {
       return queue;
     }
   });
@@ -74,31 +76,67 @@ const findQueueData = ( queuesData: object[], queueRequested: string): object=>{
 }
 // Return summoner id for now
 const getStats = async (req: Request, res: Response) => {
-  if(req.params.queueId !== "Solo" && req.params.queueId !== "Flex"){
+  if (req.params.queueId !== "Solo" && req.params.queueId !== "Flex") {
     res.json({
       message: "Bad Request",
-  }).status(400)
-  }else{
-    getEncryptID(req.params.id, req.params.region).then(({id})=>{
-      getRankedStatsID(id, req.params.region).then((result: object[])=>{
+    }).status(400)
+  } else {
+    getSummonerInfo(req.params.id, req.params.region).then(({ id }) => {
+      getRankedStatsID(id, req.params.region).then((result: object[]) => {
         const data: DataToReturn = findQueueData(result, req.params.queueId);
-        if(Object.keys(data).length === 0){
-          data.rank ="Unranked";
+        if (Object.keys(data).length === 0) {
+          data.rank = "Unranked";
           data.summonerId = id;
           data.summonerName = req.params.id;
         }
         res.set('content-location', `/api/v1/league/stats/${req.params.queueId}/${req.params.region}/${req.params.id}`).json({
-          url : `/api/v1/league/stats/${req.params.queueId}/${req.params.region}/${req.params.id}`,
+          url: `/api/v1/league/stats/${req.params.queueId}/${req.params.region}/${req.params.id}`,
           data
         }).status(201);
       })
-    }).catch((error)=>{
+    }).catch((error) => {
       res.json({
         message: "Not summoner found",
         error: error.message
-    }).status(404);
+      }).status(404);
     });
   }
 };
 
-export { getStats};
+const getMatchInfo = async (matchID: string, region: string) => {
+  return ((await axios.get(`https://${region}.api.riotgames.com/lol/match/v5/matches/${matchID}`, {
+    headers:
+    {
+      "X-Riot-Token": process.env.riotAPIKey
+    },
+    params: {
+    }
+  })).data);
+}
+
+const getMatchHistory = async (req: Request, res: Response) => {
+  let startTimeStamp = SEASON_12_BEGINS_TIMESTAMP;
+  let keepLooking: boolean = true;
+  let matchHistory: string[] = [];
+  let requestForMatchs = async (queue: number, startTime: number, puuid: string, index: number) => {
+    return (await axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?startTime=${startTime}&queue=${queue}&start=${index}&count=${100}`, {
+      headers: {
+        "X-Riot-Token": process.env.riotAPIKey
+      },
+      params: {
+      }
+    }));
+  }
+  getSummonerInfo(req.params.id, req.params.region).then(async ({ puuid }) => {
+    while (keepLooking) {
+      let responseData: string[] = (await (requestForMatchs(440, startTimeStamp, puuid, matchHistory.length))).data;
+      matchHistory = [...matchHistory, ...responseData]
+      if (responseData.length < 100) {
+        keepLooking = false;
+      }
+    }
+    res.send(matchHistory);
+  })
+}
+
+export { getStats, getMatchHistory };
--- src/routes/league.ts
+++ src/routes/league.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import {getStats} from "../controllers/leagueController"
+import {getStats, getMatchHistory} from "../controllers/leagueController"
 const router = express();
 
 router.get('/stats/:queueId/:region/:id', getStats);
+router.get('/match/:queueId/:region/:id', getMatchHistory);
 
 export default router;
